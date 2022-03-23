0e458037594b377c62d3d27e97ba9b87cba56b84
MB2M <damsbooster@gmail.com>
2022-03-23 10:37:10 +0100

    up

--- station/Station.js
+++ station/Station.js
@@ -51,7 +51,6 @@ class Station {
                         type: measurement.type,
                         shortcut,
                     },
-                    false
                 );
 
                 if (isFinal) {

90de8da4463bb72e0086b998387341100c10dedf
MB2M <aphal.dev@gmail.com>
2023-07-22 23:37:59 +0200

    up

--- client/pages/warmup/index.tsx
+++ client/pages/warmup/index.tsx
@@ -1,4 +1,4 @@
-import { useEventContext } from "../../../context/event";
+import { useEventContext } from "../../context/event";
 
 const Warmup = () => {
     const { eventId } = useEventContext();

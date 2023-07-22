fdbe945cfa7b7a11941a8a563d4c576c624d9efc
MB2M <aphal.dev@gmail.com>
2023-07-22 23:28:47 +0200

    up

--- client/pages/mandelieu/warmup/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useEventContext } from "../../../context/event";
-
-const Warmup = () => {
-    const { eventId } = useEventContext();
-
-    return <h1>{eventId}</h1>;
-};
-
-export default Warmup;

1b26fa2049490e2534a044883fe6ab06d4054645
MB2M <aphal.dev@gmail.com>
2023-08-30 19:05:28 +0200

    up

--- client/context/competition/model.ts
+++ client/context/competition/model.ts
@@ -1,9 +1,10 @@
 export const DEFAULT_COMPETITION_CONTEXT_VALUE: Competition = {
+    customFont: "BebasNeue",
     _id: "",
     name: "",
     state: "active",
     platform: "CompetitionCorner",
     eventId: "",
     selected: true,
-    workouts: []
+    workouts: [],
 };

7ff02033df7ddd4728645d7b75ee9f9e40c00a7b
MB2M <aphal.dev@gmail.com>
2023-09-03 15:00:44 +0200

    up

--- src/mtWod1/services/firebase/admin/adminConfig.ts
+++ src/mtWod1/services/firebase/admin/adminConfig.ts
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import admin from "firebase-admin";
 
-import serviceAccount from "./firebase-credentials.json" assert { type: "json" };
+// import serviceAccount from "./firebase-credentials.json" assert { type: "json" };
 // const serviceAccount = require(`../../../../../${process.env.FIREBASE_CREDENTIAL}.json`);
 
 // REALTIME DB
 // FIRESTORE
 if (!admin.apps.length) {
     try {
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount as any),
-            // databaseURL: process.env.FIREBASE_DB, // ONLY REALTIME DB
-        });
+        // admin.initializeApp({
+        //     credential: admin.credential.cert(serviceAccount as any),
+        //     // databaseURL: process.env.FIREBASE_DB, // ONLY REALTIME DB
+        // });
     } catch (error) {
         console.log("Firebase admin initialization error", error);
     }

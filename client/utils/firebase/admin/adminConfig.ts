d51e73f39ab33104d0eae95d7d5b066a1cce5cf6
MB2M <aphal.dev@gmail.com>
2023-09-03 14:52:59 +0200

    up

--- client/utils/firebase/admin/adminConfig.ts
+++ client/utils/firebase/admin/adminConfig.ts
@@ -1,35 +1,35 @@
-// Import the functions you need from the SDKs you need
-import admin from "firebase-admin";
-
-import serviceAccount from "./firebase-credentials.json" assert { type: "json" };
-
-
-if (!admin.apps.length) {
-    try {
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount as any),
-            databaseURL: process.env.FIREBASE_DB,
-            // databaseAuthVariableOverride: {
-            //     uid: "exos-server",
-            // },
-        });
-    } catch (error) {
-        console.log("Firebase admin initialization error", error);
-    }
-}
-// const initAdminApp = (apiKey: string) => {
-
-//     admin.initializeApp(
-//         {
+// // Import the functions you need from the SDKs you need
+// import admin from "firebase-admin";
+//
+// import serviceAccount from "./firebase-credentials.json" assert { type: "json" };
+//
+//
+// if (!admin.apps.length) {
+//     try {
+//         admin.initializeApp({
 //             credential: admin.credential.cert(serviceAccount as any),
 //             databaseURL: process.env.FIREBASE_DB,
-//             databaseAuthVariableOverride: {
-//                 apiKey: apiKey,
-//             },
-//         },
-//         apiKey
-//     );
-//     return admin;
-// };
-
-export default admin;
+//             // databaseAuthVariableOverride: {
+//             //     uid: "exos-server",
+//             // },
+//         });
+//     } catch (error) {
+//         console.log("Firebase admin initialization error", error);
+//     }
+// }
+// // const initAdminApp = (apiKey: string) => {
+//
+// //     admin.initializeApp(
+// //         {
+// //             credential: admin.credential.cert(serviceAccount as any),
+// //             databaseURL: process.env.FIREBASE_DB,
+// //             databaseAuthVariableOverride: {
+// //                 apiKey: apiKey,
+// //             },
+// //         },
+// //         apiKey
+// //     );
+// //     return admin;
+// // };
+//
+// export default admin;
